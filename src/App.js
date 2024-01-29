d3eda921c21dcb212247d81cd60d891cdaba0eb0
YasSerHbh <hbhyasser@gmail.com>
2024-01-29 17:57:35 +0100

    Added Main files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+    const [advice, setAdvice] = useState();
+
+    useEffect(() => {
+      fetchAdvice()
+    }, [])
+
+
+    const fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((res) => {
+            setAdvice(res.data.slip.advice)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    
+    const translate = async () => {
+        if (advice.includes('a') || advice.includes('e') || advice.includes('u')) {
+        const options = {
+            method: 'POST',
+            url: 'https://rapid-translate-multi-traduction.p.rapidapi.com/t',
+            headers: {
+              'content-type': 'application/json',
+              'X-RapidAPI-Key': 'dbc9a9236bmsh739de702e5fe09ep1e57eejsn90ea05c93668',
+              'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com'
+            },
+            data: {
+              from: 'en',
+              to: 'ar',
+              q: advice
+            }
+          };
+          
+          try {
+              const response = await axios.request(options);
+                setAdvice(response.data[0])
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container con'>
+            <div className="card">
+            <h2 className='title'>Quote of the day</h2>
+                <h1 className="heading">{advice}</h1>
+                <p className='translate' onClick={translate}>Translate</p>
+            </div>
+        </div>
+        <p className='copyright'>Created by <a href='https://facebook.com/YasSer.0726' rel='noreferrer' target='_blank' >YasSer</a></p>
     </div>
   );
 }
